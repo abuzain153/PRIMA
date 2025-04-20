cc3826312413b01d0aa7cdb5f6c939fdc04df013
abuzain153 <abuzain186@gmail.com>
2025-04-20 12:52:34 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,6 +75,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # تم إضافة هذا السطر
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myapp/static'),
 ]
