1cabe91efc971a2830daae7e77c003e20d8d84fa
abuzain153 <abuzain186@gmail.com>
2025-04-20 14:06:50 +0300

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from myapp import views
+from myapp import views # استيراد views من myapp عشان لو login_view هناك
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),
-    path('login/', views.login_view, name='login'),  # إضافة مسار تسجيل الدخول هنا
-    path('', views.product_list, name='home'),
+    path('login/', views.login_view, name='login'),   # لو login_view في myapp/views.py
+    path('', include('myapp.urls')), # الـ URL الرئيسي هيشمل URLs تطبيق myapp
 ]
